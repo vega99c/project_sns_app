7b46decbf7a574ecbc35bf6fca97c963a2ef0606
vega99c <vega99c@naver.com>
2023-11-17 18:57:04 +0900

    add: 유저정보 및 포스팅, 댓글 기능을 위한 모델 작성

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -19,8 +19,43 @@ const userSchema = mongoose.Schema({
         type: String,
         unique: true,
         sparse: true
-    }
-})
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    firstName: {
+        type: String,
+        default: 'First Name'
+    },
+    lastName: {
+        type: String,
+        default: 'last Name'
+    },
+    bio: {
+        type: String,
+        default: '데이터 없음'
+    },
+    hometown: {
+        type: String,
+        default: '데이터 없음'
+    },
+    workplace: {
+        type: String,
+        default: '데이터 없음'
+    },
+    education: {
+        type: String,
+        default: '데이터 없음'
+    },
+    contact: {
+        type: Number,
+        default: 123456789
+    },
+    friends: [{ type: String }],
+    friendRequests: [{ type: String }]
+}, {timestamps: true })
 
 const saltRounds = 10;
 userSchema.pre('save', function (next) {
