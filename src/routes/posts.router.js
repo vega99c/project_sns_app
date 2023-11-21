5d2cf8dd7f767c277fcaa4d1e90aaee756ccd4a3
vega99c <vega99c@naver.com>
2023-11-21 13:36:36 +0900

    add: 포스트 페이지 라우팅위한 일부코드 추가

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { checkAuthenticated } = require('../middleware/auth');
+
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('posts');
+})
 
 module.exports = router;
