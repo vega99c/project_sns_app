68cc518aab306009e5ff4ff8463935e1852a6009
vega99c <vega99c@naver.com>
2023-11-27 13:22:30 +0900

    feat(posts/router): 포스트 수정 기능 구현
    - edit버튼 누를 시 수정페이지로 라우팅
    - edit페이지에서 업데이트 된 본문내용을 put메서드방식으로 다시 게시물을 업데이트한다.
    - server.js에서도 method-override모듈을 꼭 등록 해줘야 한다.

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { checkAuthenticated } = require('../middleware/auth');
+const { checkAuthenticated, checkPostOwnership } = require('../middleware/auth');
 const Post = require('../models/posts.model');
 const Comment = require('../models/comments.model');
 const multer = require('multer');
 const path = require('path');
+
 
 const storageEngine = multer.diskStorage({
     destination: (req, file, callback) => {
@@ -38,6 +39,24 @@ router.post('/', checkAuthenticated, upload, (req, res, next) => {
             res.redirect("back");
         }
     })   
+})
+
+router.get("/:id/edit", checkPostOwnership, (req, res) => {    
+    res.render("posts/edit", {
+        post: req.post
+    })        
+})
+
+router.put('/:id', checkPostOwnership, (req,res) => {
+    Post.findByIdAndUpdate(req.params.id, req.body, (err, _) => {
+        if(err) {
+            req.flash('error', '게시물을 수정하는데 오류가 발생했습니다.');
+            res.redirect('/posts');            
+        } else {
+            req.flash('success', '게시물 수정을 완료했습니다,');
+            res.redirect('/posts');
+        }
+    })
 })
 
 router.get('/', checkAuthenticated, (req, res) => {
